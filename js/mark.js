26560c0178a8a9a319fddbad4475cc3fb28ee3a6
Kriegslustig <github@ls7.ch>
2014-12-20 00:21:17 +0100

    Adding basic highlighting functionality

---
+++ js/mark.js
@@ -0,0 +1,78 @@
+window.addEventListener('load', function () {
+
+  var markElem = createMarkElement();
+  markElem.init(document.querySelector('.markdown__textarea'));
+
+}, false);
+
+var createMarkElement = (function () {
+  var _markSyntax = [
+    {
+      description: 'Bold text',
+      regex: /^(#\ ([^\n$]*))/gm,
+      tag: function ($0, $1) {
+        return '<b>' + $1 + '</b>';
+      }
+    },
+    {
+      description: 'Linebreaks',
+      regex: /\n/g,
+      tag: '<br>'
+    },
+    {
+      description: 'Double spaces',
+      regex: /\ \ /g,
+      tag: '&nbsp;&nbsp;'
+    },
+    {
+      description: 'Italic text',
+      regex: /\ (\*[^\n\*$]*\*)/g,
+      tag: function ($0, $1) {
+        return ' <i>' + $1 + '</i>';
+      }
+    },
+    {
+      description: 'Code blocks',
+      regex: /(^|\ )```([^$]*)```/gm,
+      tag: function ($0,$1,$2) {
+        return $1 + '```<code>' + $2 + '</code>```';
+      }
+    },
+  ],
+  _node,
+  _cover;
+
+  function _checkSyntax () {
+    var content = _node.value;
+    _markSyntax.forEach(function (val, index, array) {
+      var match = true;
+
+      while ( match ){
+        newContent = content.replace(val.regex, val.tag);
+        if(newContent === content) {
+          break;
+        } else {
+          content = newContent;
+        }
+      };
+    });
+    _cover.innerHTML = content;
+  }
+
+  function _setListeners () {
+    document.addEventListener('keydown', function () {
+      setTimeout( function () {
+        _checkSyntax();
+      }, 20);
+    }, false);
+  }
+
+  return {
+    init: function (node) {
+      _node = node;
+      _cover = document.querySelector('.markdown__cover');
+      _checkSyntax();
+      _setListeners();
+    },
+  }
+});
