ad95b4d72020eeea6d1d62ea8559180233d67685
Kriegslustig <github@ls7.ch>
2014-12-20 02:04:10 +0100

    Refactoring the syntac definition to be in a seperate file

--- js/mark.js
+++ js/mark.js
@@ -1,59 +1,12 @@
 window.addEventListener('load', function () {
-
-  var markElem = createMarkElement();
   markElem.init(document.querySelector('.markdown__textarea'));
   document.querySelector('.markdown__textarea').focus();
-
 }, false);
 
 var createMarkElement = (function () {
-  var _markSyntax = [
-    {
-      description: 'Header 1 text',
-      regex: /^(#\ ([^\n$]*))/gm,
-      tag: function ($0, $1) {
-        return '<b>' + $1 + '</b>';
-      }
-    },
-    {
-      description: 'Italic text',
-      regex: /\ (\*[^\n\*$]*\*)/g,
-      tag: function ($0, $1) {
-        return ' <i>' + $1 + '</i>';
-      }
-    },
-    {
-      description: 'Code block',
-      regex: /(^|\ )`{3}\n([^$]*)\n`{3}/gm,
-      tag: function ($0,$1,$2) {
-        return $1 + '&#0096;&#0096;&#0096;\n<code class="code_block">' + $2 + '</code>\n&#0096;&#0096;&#0096;';
-      }
-    },
-    {
-      description: 'Inline code',
-      regex: /(^|\ )`{3}([^$]*)`{3}/gm,
-      tag: function ($0,$1,$2) {
-        return $1 + '&#0096;&#0096;&#0096;<code>' + $2 + '</code>&#0096;&#0096;&#0096;';
-      }
-    },
-    {
-      description: 'Spaces after linebreaks',
-      regex: /^\ /gm,
-      tag: '&nbsp;'
-    },
-    {
-      description: 'Linebreaks',
-      regex: /\n/gm,
-      tag: '<br>'
-    },
-    {
-      description: 'Double spaces',
-      regex: /\ (?=\ )/g,
-      tag: '&nbsp;'
-    },
-  ],
-  _node,
-  _cover;
+  var _node,
+  _cover,
+  _markSyntax = [];
 
   function _checkSyntax () {
     var content = _node.value;
@@ -87,5 +40,14 @@ var createMarkElement = (function () {
       _checkSyntax();
       _setListeners();
     },
+    addSyntax: function (pushThis) {
+      if(Array.isArray(pushThis)) {
+        _markSyntax = _markSyntax.concat(pushThis);
+      } else {
+        _markSyntax.push(pushThis);
+      }
+    },
   }
 });
+
+var markElem = createMarkElement();
---
+++ js/mdSyntax.js
@@ -0,0 +1,45 @@
+markElem.addSyntax([
+  {
+    description: 'Header 1 text',
+    regex: /^(#\ ([^\n$]*))/gm,
+    tag: function ($0, $1) {
+      return '<b>' + $1 + '</b>';
+    }
+  },
+  {
+    description: 'Italic text',
+    regex: /\ (\*[^\n\*$]*\*)/g,
+    tag: function ($0, $1) {
+      return ' <i>' + $1 + '</i>';
+    }
+  },
+  {
+    description: 'Code block',
+    regex: /(^|\ )`{3}\n([^$]*)\n`{3}/gm,
+    tag: function ($0,$1,$2) {
+      return $1 + '&#0096;&#0096;&#0096;\n<code class="code_block">' + $2 + '</code>\n&#0096;&#0096;&#0096;';
+    }
+  },
+  {
+    description: 'Inline code',
+    regex: /(^|\ )`{3}([^$]*)`{3}/gm,
+    tag: function ($0,$1,$2) {
+      return $1 + '&#0096;&#0096;&#0096;<code>' + $2 + '</code>&#0096;&#0096;&#0096;';
+    }
+  },
+  {
+    description: 'Spaces after linebreaks',
+    regex: /^\ /gm,
+    tag: '&nbsp;'
+  },
+  {
+    description: 'Linebreaks',
+    regex: /\n/gm,
+    tag: '<br>'
+  },
+  {
+    description: 'Double spaces',
+    regex: /\ (?=\ )/g,
+    tag: '&nbsp;'
+  },
+])
