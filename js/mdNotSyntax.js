0f05e197459658db306044e691cbc5f801813af6
Kriegslustig <github@ls7.ch>
2014-12-22 17:54:09 +0100

    Adding view support to all syntax entities

--- js/mdNotSyntax.js
+++ js/mdNotSyntax.js
@@ -2,40 +2,55 @@ for (var i = 1; i < 6; i++) {
   markElem.addSyntax({
     description: 'Header ' + i + ' text',
     regex: new RegExp('^(#{' + i + '}\ ([^\n$]*))(\n|$)', 'gm'),
-    tag: function ($0, $1) {
+    tag: function ($0, $1, $2, $3) {
       return '<h' + i + '>' + $1 + '</h' + i + '>';
-    }
+    },
+    view: function ($0, $1, $2, $3) {
+      return '<h' + i + '>' + $2 + '</h' + i + '>';
+    },
   });
 };
 
 markElem.addSyntax([
   {
     description: 'Bold text',
-    regex: /(^|\ )(\*\*[^\n\*$]*\*\*)/gm,
-    tag: function ($0, $1, $2) {
+    regex: /(^|\ )(\*\*([^\n\*$]*)\*\*)/gm,
+    tag: function ($0, $1, $2, $3) {
       return $1 + '<b>' + $2 + '</b>';
-    }
+    },
+    view: function ($0, $1, $2, $3) {
+      return $1 + '<b>' + $3 + '</b>';
+    },
   },
   {
     description: 'Italic text',
-    regex: /(^|\ )(\*[^\n\*$]*\*)/gm,
-    tag: function ($0, $1, $2) {
+    regex: /(^|\ )(\*([^\n\*$]*)\*)/gm,
+    tag: function ($0, $1, $2, $3) {
       return $1 + '<i>' + $2 + '</i>';
+    },
+    view: function ($0, $1, $2, $3) {
+      return $1 + '<i>' + $3 + '</i>';
     }
   },
   {
     description: 'Code block',
-    regex: /(^|\ )`{3}\n([^$]*)\n`{3}/gm,
-    tag: function ($0,$1,$2) {
+    regex: /(^)`{3}\n([^$]*)\n`{3}/gm,
+    tag: function ($0, $1, $2) {
       return $1 + '&#0096;&#0096;&#0096;\n<code class="code_block">' + $2 + '</code>\n&#0096;&#0096;&#0096;';
-    }
+    },
+    view: function ($0, $1, $2) {
+      return $1 + '<code class="code_block">' + $2 + '</code>';
+    },
   },
   {
     description: 'Inline code',
     regex: /(^|\ )`{3}([^$]*)`{3}/gm,
-    tag: function ($0,$1,$2) {
+    tag: function ($0, $1, $2) {
       return $1 + '&#0096;&#0096;&#0096;<code>' + $2 + '</code>&#0096;&#0096;&#0096;';
-    }
+    },
+    view: function ($0, $1, $2) {
+      return $1 + '<code>' + $2 + '</code>';
+    },
   },
   {
     description: 'Images',
@@ -45,7 +60,7 @@ markElem.addSyntax([
     },
     view: function ($0, $1, $2, $3) {
       return $1 + '<img alt="' + $2 + '" src="' + $3 + '">';
-    }
+    },
   },
   {
     description: 'Links',
